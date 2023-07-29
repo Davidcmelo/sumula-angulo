154d255a580ebcfcd99c64624982f241bb70021d
David Cardoso <davidcmelo99@hotmail.com>
2023-07-29 11:04:56 -0300

    update d.ts

--- pdf.d.ts
+++ pdf.d.ts
@@ -1,4 +1,4 @@
 declare module 'html2pdf.js' {
-    function html2pdf(element: HTMLElement, opt?: any);
+    function html2pdf(element: HTMLElement, options?: any);
     export default html2pdf;
   }
